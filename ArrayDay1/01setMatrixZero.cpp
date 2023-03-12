6c2cbd1fc0367fe08ef62d945486ee0c07592278
shubhampatel27 <shubhampatel0305@gmail.com>
2023-03-12 23:44:30 +0530

    Question01

---
+++ ArrayDay1/01setMatrixZero.cpp
@@ -0,0 +1,30 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int rowSize= matrix.size();
+        int colSize = matrix[0].size();
+         int col0 = 1;
+        for(int row =0; row< rowSize; row++){
+            if ( matrix[row][0] ==0 ) col0 = 0;
+            for(int col=1; col <colSize ; col++){
+              if(matrix [row][col] ==0){
+                  matrix[row][0] = matrix[0][col] =0;
+              }
+            }
+        }
+
+        for(int row = rowSize -1; row >=0; row--){
+            for(int col = colSize-1; col >0; col--){
+                if( matrix[row][0]== 0|| matrix[0][col] ==0)
+                  matrix[row][col] = 0;   
+                   
+            }
+            if(col0 ==0)
+                  matrix[row][0]=0;
+             
+        }
+    }
+};
